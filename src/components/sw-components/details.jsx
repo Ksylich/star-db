7d125065b3d862cd0fb7bef0f3bd3a34ce31fcba
ksylich <okselenkoandrej@gmail.com>
2020-01-31 15:00:47 +0200

    palnet details

--- src/components/sw-components/details.jsx
+++ src/components/sw-components/details.jsx
@@ -24,17 +24,29 @@ const PersonDetails = props => {
   );
 };
 
-const StarshipsDetails= props => {
-    return (
-      <Deatails {...props} getData={getStarship} getImage={getStarshipImage}>
-       <Record field="model" label="Model:" />
-        <Record field="costInCredits" label="Cost:" />
-        <Record field="crew" label="Crew:" />
-        <Record field="passengers" label="Passengers:" />
-        <Record field="hyperdriveRating" label="Hyperdrive Rating:" />
-      </Deatails>
-    );
-  };
-const PlanetsDetails = WithData(ItemDetails);
+const StarshipsDetails = props => {
+  return (
+    <Deatails {...props} getData={getStarship} getImage={getStarshipImage}>
+      <Record field="model" label="Model:" />
+      <Record field="costInCredits" label="Cost:" />
+      <Record field="crew" label="Crew:" />
+      <Record field="passengers" label="Passengers:" />
+      <Record field="hyperdriveRating" label="Hyperdrive Rating:" />
+    </Deatails>
+  );
+};
+const PlanetsDetails  = props => {
+  return (
+    <Deatails {...props} getData={getStarship} getImage={getStarshipImage}>
+      <Record field="name" label="Name:" />
+      <Record field="climate" label="Climate:" />
+      <Record field="diameter" label="Diameter:" />
+      <Record field="population" label="Population:" />
+      <Record field="rotationPeriod" label="Rotation period:" />
+      <Record field="terrain" label="Terrain:" />
+
+    </Deatails>
+  );
+};
 
 export { PersonDetails, StarshipsDetails, PlanetsDetails };
